a32115476c8a6993d044a23861d69dc632e9e0b7
sneji <sneji@vermeg.com>
2022-04-14 23:25:16 +0100

    Contract Service added

--- src/main/java/tn/esprit/spring/services/ContractServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContractServiceImpl.java
@@ -15,6 +15,7 @@ public class ContractServiceImpl implements IContractService {
     ContratRepository contratRepository;
     
     public List<Contrat> getAllContrats() {
+
         return (List<Contrat>) contratRepository.findAll();
     }
 
